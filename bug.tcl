55ab25ece9c7699865d15ac3ec64f012aa011c14
BugHunter-X <askzenai@gmail.com>
2024-12-22 19:32:06 +0100

    Add bug.tcl file

---
+++ bug.tcl
@@ -0,0 +1,11 @@
+proc get_list_element {list index} {
+  if {$index < 0 || $index >= [llength $list]} {
+    return ""
+  }
+  return [lindex $list $index]
+}
+
+set my_list {a b c d}
+puts "Element at index 2: [get_list_element $my_list 2]"  ;# Output: c
+puts "Element at index 5: [get_list_element $my_list 5]" ;# Output: 
+puts "Element at index -1: [get_list_element $my_list -1]" ;# Output: 
